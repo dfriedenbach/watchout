378c87c29b61f11f31357aa73dfa1740bfdd1d20
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-10 16:19:01 -0700

    Add draggable player circle

--- client/watchout.js
+++ client/watchout.js
@@ -10,12 +10,12 @@ for(var i = 0 ; i < 10; i++){
 var d3Enemies = d3.select('.gameboard').selectAll('.asteroid').data(enemies);
 
 d3Enemies.enter()
-    .append('circle')
-    .attr('class', 'asteroid')
-    .attr('cx', function(d){return d.x})
-    .attr('cy', function(d){return d.y})
-    .attr('r', 25)
-    .style('fill', 'purple');
+  .append('circle')
+  .attr('class', 'asteroid')
+  .attr('cx', function(d){return d.x})
+  .attr('cy', function(d){return d.y})
+  .attr('r', 25)
+  .style('fill', 'black');
 
 setInterval(function(){
   d3Enemies.transition()
@@ -23,3 +23,27 @@ setInterval(function(){
     .attr('cx', function(d){return Math.random() * $('.gameboard').width()})
     .attr('cy', function(d){return Math.random() * $('.gameboard').height()})
 }, 1000);
+
+var player = {
+  x: $('.gameboard').width() / 2,
+  y: $('.gameboard').height() / 2,
+  id: 'player'
+}
+var drag = d3.behavior.drag();
+drag.on('drag', function(d){
+  d3.select(this)
+    .attr('cx', d.x = d3.event.x)
+    .attr('cy', d.y = d3.event.y);
+});
+
+
+d3.select('.gameboard').selectAll('.player')
+    .data([player])
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){return d.x})
+    .attr('cy', function(d){return d.y})
+    .attr('r', 25)
+    .style('fill', 'purple')
+    .call(drag);
+
